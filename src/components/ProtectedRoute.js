e0935412e66841026601fc0da797dfdf246db48b
ajit kumar <107497826+Ajit-ng@users.noreply.github.com>
2024-07-12 08:20:33 +0530

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,1 +1,18 @@
 
+
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./components/firebase";
+
+const ProtectedRoute = ({ children }) => {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return user ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
